15ed905cadbc02adb01d46332f849dccc2741ae7
Sergio Mendieta <mendiets@uci.edu>
2024-08-11 10:02:46 -0700

    Content Additions to Webpages
    Added appropriete content to Home, Register, and Courses Pages. Next step is to see about submitting form information as that is what the focus is as of right now for the project.

--- makerspace_sign-in/src/Components/EquipmentPage.js
+++ makerspace_sign-in/src/Components/EquipmentPage.js
@@ -4,7 +4,8 @@ class EquipmentPage extends Component {
   render() {
     return (
       <div className='PageContent'>
-      <Content Header={"Equipment"}/>
+      <Content Header={"Equipment"} Text={<p>Our MakerSpace has a wide range of equipment for students, faculty, and staff to use.<br />Equipment is used on a first-come, first-serve basis.
+      </p>}/>
     </div>
     )
   }
--- makerspace_sign-in/src/Components/RegisterPage.js
+++ makerspace_sign-in/src/Components/RegisterPage.js
@@ -5,7 +5,74 @@ class RegisterPage extends Component {
   render() {
     return (
       <div className='PageContent'>
-      <Content Header={"Register"}/>
+      <Content Header={"Register"} Text={<p>The MakerSpace implaments a Check-in/Check-out to track the use of the MakerSpace in efforts demonstrate its necessity on campus. 
+        Registering allows you to keep up to date with the latest in Moorpark College MakerSpace news and assists us in pioneering efforts to get a larger space, more tools and more materials,
+        all for your use!
+        {/*<span style={{fontWeight:"bold"}}> All incoming users must register!</span>*/}
+      </p>}/>
+      <div className='Form'>
+        <h1 className='Header'> Sign Up!</h1>
+        <form >
+        
+        <div className='RegisterForm' style={{fontFamily:"Zilla Slab"}}>
+          
+          <label>First Name</label><input type="text" /><br />
+          <label>Last Name</label><input type="text" /><br />
+          <label>Email</label> <input type="text" /><br />
+          <label>Student ID</label> <input type="text" /><br />
+          <label>Major </label> 
+          <select name='Major'>
+          <option value={"Accounting"}> Accounting </option>
+          <option value={"Aerospace Engineering"}> Aerospace Engineering </option>
+          <option value={"African American Studies"}> African American Studies </option>
+          <option value={"Anthropology"}> Anthropology </option>
+          <option value={"Art History"}> Art History </option>
+          <option value={"Asian American Studies"}> Asian American Studies </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          <option value={"CS"}> Computer Science </option>
+          </select><br />
+          <input type="submit"/>
+          
+          
+        </div>
+
+      </form>
+      </div>
+      
+
     </div>
     )
   }
