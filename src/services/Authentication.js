dffd4ec3306eef808cb0d3b920356d0799ecab34
AcangLX <lancexia2001@gmail.com>
2025-03-05 16:58:47 -0800

    fix(register-user): dateofbirth format issue

--- src/services/Authentication.js
+++ src/services/Authentication.js
@@ -3,13 +3,14 @@ import { apiClient } from "utils/ApiClient";
 import { handleApiRequest } from "utils/ApiUtils";
 
 export const handleRegister = async (values) => {
+  const dateOfBirth = new Date(values.birthday.year, values.birthday.month - 1, values.birthday.day);
   const payload = {
     lastName: values.lastName,
     firstName: values.firstName,
     email: values.email,
     password: values.password,
     major: values.major,
-    dateOfBirth: values.birthday,
+    dateOfBirth: dateOfBirth,
     code: values.confirm
   };
   return handleApiRequest(() =>
